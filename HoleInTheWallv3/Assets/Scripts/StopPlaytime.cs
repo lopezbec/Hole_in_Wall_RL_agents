773d038cddf2d7342c9283126235f361facfaeb1
Whiiizz <hongw@lafayette.edu>
2026-01-13 12:52:07 -0500

    More Test Walls

--- HoleInTheWallv3/Assets/Scripts/StopPlaytime.cs
+++ HoleInTheWallv3/Assets/Scripts/StopPlaytime.cs
@@ -46,10 +46,10 @@ public class StopPlaytime : MonoBehaviour
         //trigger the alarm if the pattern persists
         if (flag >= 3)
         {
-#if UNITY_EDITOR
-            Debug.LogError("Stopping Play Mode due to collapsing reward");
-            EditorApplication.isPlaying = false;
-#endif
+// #if UNITY_EDITOR
+//             Debug.LogError("Stopping Play Mode due to collapsing reward");
+//             EditorApplication.isPlaying = false;
+// #endif
         }
 
         agent_amt = 0;
@@ -66,15 +66,16 @@ public class StopPlaytime : MonoBehaviour
         float drop_tolerance = 2f;
         float similar_tolerance = 0.001f;
 
-        //collapse patterns: previous reward is positive, now becomes negative with a huge jump. OR previous reward is now very similar to current reward multiple times while negative, indicating the agent is now stuck in the same action
-        if ((previous_reward >= 0 && current_reward <= 0 && difference >= difference_tolerance) || (current_reward <= 0 && previous_reward <= 0 && Math.Abs(previous_reward - current_reward) <= similar_tolerance))
-        {
-            return true;
-        }
+        //collapse patterns: previous reward is positive, now becomes negative with a huge jump. 
+        if ((previous_reward >= 0 && current_reward <= 0 && difference >= difference_tolerance)) return true;
+        //OR previous reward is now very similar to current reward multiple times while negative, indicating the agent is now stuck in the same action
+        if ((current_reward <= 0 && previous_reward <= 0 && Math.Abs(previous_reward - current_reward) <= similar_tolerance)) return true;
 
         //if there is a sharp drop when it was positive, instant cancel run
-        if(max_seen - current_reward >= drop_tolerance && max_seen >= 0) {
+        if (max_seen - current_reward >= drop_tolerance && max_seen >= 0)
+        {
             flag = 3;
+            Debug.Log("Collapsed due to positive to negative large drop");
             return true;
         }
 
